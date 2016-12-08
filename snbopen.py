1530118dc245329b4819a6db4118452fe55ee6cf
Ohad Cohen <ohadcn@gmail.com>
2016-12-08 08:05:05 +0200

    add Image.fromstring() as fallback

--- snbopen.py
+++ snbopen.py
@@ -34,7 +34,10 @@ def addImage(snbFile,canvas,image,rels,element):
 		imgStr = decompress(imgStr)
 		width = ord(imgStr[5]) * 256 + ord(imgStr[4])
 		height = ord(imgStr[9]) * 256 + ord(imgStr[8])
-		img = Image.frombytes("RGBA",(width,height),imgStr[52:])
+		try:
+			img = Image.frombytes("RGBA",(width,height),imgStr[52:])
+		except:
+			img = Image.fromstring("RGBA",(width,height),imgStr[52:])
 		canvas.drawInlineImage(alpha_to_color(img),0,0,595.27,841.89)
 	else:
 		style = imagePoss(element.getElementsByTagName("v:shape")[0].getAttribute("style"))
